0f0e609989e141ef8c5541183357003c7c8799f6
Amit Thawait <amit17thawait@gmail.com>
2015-07-21 18:49:40 +0530

    Added tag creation and linking code

--- rails/app/models/bookmark.rb
+++ rails/app/models/bookmark.rb
@@ -4,14 +4,16 @@ class Bookmark < ActiveRecord::Base
 
   has_many :tags, through: :taggings
 
+  # after_save :associate_tags
+
   def all_tags=(names)
-    self.tags = names.split(",").map do |name|
+    self.tags = names.split(',').map do |name|
       Tag.where(name: name.strip).first_or_create!
     end
   end
 
   def all_tags
-    self.tags.map(&:name).join(", ")
+    self.tags.map(&:name).join(', ')
   end
 
 end
