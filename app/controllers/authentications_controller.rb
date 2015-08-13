03d28f6ca74d552ff18a01abb40055849e06339c
Amit Thawait <amit17thawait@gmail.com>
2015-08-13 18:53:51 +0530

    Added authentications table and stored user info given by provider

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,7 +1,24 @@
 class AuthenticationsController < ApplicationController
 
   def create
-    render :plain => request.env["omniauth.auth"].inspect
+    # render :plain => request.env["omniauth.auth"].inspect
+    auth = request.env["omniauth.auth"]
+    session[:access_token] = auth["credentials"]["token"]
+    session[:provider_userid] = auth['uid']
+    @authentication = Authentication.find_or_create_by(provider: auth['provider'], uid: auth['uid'])
+    if @authentication
+      flash[:notice] = "Signed in successfully."
+    else
+      raise "User could not be authenticated. Please Try again"
+    end
+    redirect_to bookmarks_url
+  end
+
+  def destroy
+    authentication = Authentication.find(params[:id])
+    authentication.destroy if @authentication
+    flash[:notice] = "Successfully destroyed authentication."
+    redirect_to authentications_url
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150715070943) do
+ActiveRecord::Schema.define(version: 20150806135233) do
+
+  create_table "authentications", force: :cascade do |t|
+    t.string   "provider",   limit: 255
+    t.string   "uid",        limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "bookmarks", force: :cascade do |t|
     t.string   "name",        limit: 255
