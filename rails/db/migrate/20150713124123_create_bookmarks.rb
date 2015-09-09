09b23a1b8d6f5ca788ff4de10600dd3dce735402
Amit Thawait <amit17hawait@gmail.com>
2015-09-09 17:09:02 +0530

    Corrected left padding & made name as required field

--- rails/db/migrate/20150713124123_create_bookmarks.rb
+++ rails/db/migrate/20150713124123_create_bookmarks.rb
@@ -1,7 +1,7 @@
 class CreateBookmarks < ActiveRecord::Migration
   def change
     create_table :bookmarks do |t|
-      t.string :name
+      t.string :name, null: false
       t.string :url, null: false
       t.string :description
 
