72499fcf8bf4532bc30f6c33e41ffa6ec81af68a
Amit Thawait <amit17thawait@gmail.com>
2015-10-26 11:35:32 +0530

    Referencing bookmark model instead of test class for URI validation

--- rails/spec/lib/custom_validators/uri_validator_spec.rb
+++ rails/spec/lib/custom_validators/uri_validator_spec.rb
@@ -4,51 +4,46 @@ require "#{Rails.root}/lib/custom_validators/uri_validator"
 # Source: http://gist.github.com/bf4/5320847
 # spec/validators/uri_validator_spec.rb
 describe UriValidator do
-  subject do
-    Class.new do
-      include ActiveModel::Validations
-      attr_accessor :url
-      validates :url, uri: { allowed_protocols: ['http', 'https'], disallowed_protocols: ['ftp', 'ssh']}
-    end.new
-  end
+
+  let(:bookmark) { FactoryGirl.create(:bookmark) }
 
   it "should be valid for a valid http url" do
-    subject.url = 'http://www.google.com'
-    subject.valid?
-    expect(subject.errors.full_messages).to eq([])
+    bookmark.url = 'http://www.google.com'
+    bookmark.valid?
+    expect(bookmark.errors.full_messages).to eq([])
   end
 
   ['http://.com', 'http://ftp://ftp.google.com', 'http://ssh://google.com'].each do |invalid_url|
     it "#{invalid_url.inspect} is a invalid http url" do
-      subject.url = invalid_url
-      subject.valid?
-      expect(subject.errors.full_messages).to eq([])
+      bookmark.url = invalid_url
+      bookmark.valid?
+      expect(bookmark.errors.full_messages).to eq([])
     end
   end
 
   ['http:/www.google.com', '<>hi', 'http://google'].each do |invalid_url|
     it "#{invalid_url.inspect} is an invalid url" do
-      subject.url = invalid_url
-      subject.valid?
-      expect(subject.errors.keys).to include(:url)
-      expect(subject.errors[:url]).to include("is an invalid URL")
+      bookmark.url = invalid_url
+      bookmark.valid?
+      expect(bookmark.errors.keys).to include(:url)
+      expect(bookmark.errors[:url]).to include("is an invalid URL")
     end
   end
 
   ['www.google.com','google.com'].each do |valid_url|
     it "#{valid_url.inspect} is a valid url" do
-      subject.url = valid_url
-      expect(subject.valid?).to be true
-      expect(subject.errors.full_messages).to eq([])
+      bookmark.url = valid_url
+      expect(bookmark.valid?).to be true
+      expect(bookmark.errors.full_messages).to eq([])
     end
   end
 
   ['ftp://ftp.google.com','ssh://google.com'].each do |invalid_url|
     it "#{invalid_url.inspect} is an invalid url" do
-      subject.url = invalid_url
-      subject.valid?
-      expect(subject.errors).to include(:url)
-      expect(subject.errors[:url]).to include("must begin with http or https")
+      bookmark.url = invalid_url
+      bookmark.valid?
+      expect(bookmark.errors).to include(:url)
+      expect(bookmark.errors[:url]).to include("must begin with http or https")
     end
   end
 end
